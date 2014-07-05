ec201f7a81a85cc99ad515a0e81ac0bd1bdf4d87
tramalingam <thennavan.ramalingam@careerbuilder.com>
2014-07-05 17:19:04 -0400

    first commit

---
+++ config/initializers/asserts.rb
@@ -0,0 +1,16 @@
+Rails.application.config.assets.precompile += %w( bootstrap.min.css )
+Rails.application.config.assets.precompile += %w( font-awesome.min.css )
+Rails.application.config.assets.precompile += %w( font-awesome.css)
+Rails.application.config.assets.precompile += %w( animate.min.css )
+Rails.application.config.assets.precompile += %w( prettyPhoto.css )
+Rails.application.config.assets.precompile += %w( main.css )
+Rails.application.config.assets.precompile += %w( responsive.css )
+
+
+Rails.application.config.assets.precompile += %w( jquery.js )
+Rails.application.config.assets.precompile += %w( bootstrap.min.js )
+Rails.application.config.assets.precompile += %w( jquery.prettyPhoto.js )
+Rails.application.config.assets.precompile += %w( jquery.isotope.min.js )
+Rails.application.config.assets.precompile += %w( main.js )
+Rails.application.config.assets.precompile += %w( wow.min.js )
+
