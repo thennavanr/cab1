d007552763ad9e9070837b4f2e4de62e3f11269b
tramalingam <thennavan.ramalingam@careerbuilder.com>
2014-07-18 17:54:09 -0400

    saving quick reservation

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,21 @@
 class HomeController < ApplicationController
   def index
   end
+
+  def show
+  end
+
+  def new 
+    @reservation = Reservation.new
+  end
+
+  def create
+    @reservation = Reservation.new(home_params)
+    @reservation.save
+  end
+
+  private
+  def reservation_params 
+    params.require(:reservation).permit(:name, :email, :phone)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-   root 'home#index'
+   root 'home#new'
 
    resources :reservations
    resources :contacts
