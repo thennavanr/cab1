b1aa74739ddb024b8f6cccf32899e2c56e9dbeca
Aswini <aswini.k@outlook.com>
2014-07-22 12:41:30 -0400

    email html

--- app/mailers/reservation_mailer.rb
+++ app/mailers/reservation_mailer.rb
@@ -2,6 +2,8 @@ class ReservationMailer < ActionMailer::Base
   default from: "k.aswinii@gmail.com"
 
   def register_email(reservation)
+    @reservation=reservation
+
     mail( :to => reservation.email, :subject => 'Thanks for registered.' )
   end
 
