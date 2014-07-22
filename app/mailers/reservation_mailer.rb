d3edf68f57c5ba0a4efd6a9b9aa2235cd51324ca
Aswini <aswini.k@outlook.com>
2014-07-22 15:44:23 -0400

    username removed

--- app/mailers/reservation_mailer.rb
+++ app/mailers/reservation_mailer.rb
@@ -1,5 +1,5 @@
 class ReservationMailer < ActionMailer::Base
-  default from: "k.aswinii@gmail.com"
+  default from: "'from@example.com'
 
   def register_email(reservation)
     @reservation=reservation
