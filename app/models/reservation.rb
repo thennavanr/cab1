10520d9f1f05aeb68346ae73223b24fe1f6a047d
tramalingam <thennavan.ramalingam@careerbuilder.com>
2014-08-30 22:44:12 -0400

    adding service_type column and retriving saved special request values

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,6 +1,38 @@
 class Reservation < ActiveRecord::Base
   has_many :special_requests
 
+  def get_vechile_type
+    if  special_requests.exists?(['request_type LIKE ?', "%Vechile%"])
+      special_requests.find_by(['request_type LIKE ?', "%Vechile%"]).request_value
+    else
+      2
+    end
+  end
+def get_no_passengers
+  binding.pry
+    if  special_requests.exists?(['request_type LIKE ?', "%Passengers%"])
+      special_requests.find_by(['request_type LIKE ?', "%Passengers%"]).request_value
+    else
+     1 
+    end
+end
+  def surcharge?
+    special_requests.exists?(['request_type LIKE ?', "%Surcharge%"])
+  end
+
+  def meet_greet?
+    special_requests.exists?(['request_type LIKE ?', "%Meet and Greet at Baggage Claim%"])
+  end
+  def child_seat?
+    special_requests.exists?(['request_type LIKE ?', "%Child Seat%"])
+  end 
+
+  def pets?
+    special_requests.exists?(['request_type LIKE ?', "%Pet%"])
+  end 
+
+  
+    
   def paypal_url return_url
     values = {
       :business => 'thennavanr@outlook.com',
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140822184936) do
+ActiveRecord::Schema.define(version: 20140831024231) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -46,6 +46,7 @@ ActiveRecord::Schema.define(version: 20140822184936) do
     t.boolean  "pet"
     t.string   "company_name"
     t.string   "hour"
+    t.string   "service_type"
   end
 
   create_table "special_requests", force: true do |t|
